6ea5e66363f3bf42f5e9a7419b43c58d7a7d16a1
jdyen <jdl.yen@gmail.com>
2018-03-12 14:33:18 +1100

    switch feather to R file formats

--- code/load-compadre-data.R
+++ code/load-compadre-data.R
@@ -8,9 +8,6 @@
 # optional: set working directory
 # setwd("PATH/TO/DIR")
 
-# load libraries
-library(feather)
-
 # source function to load all data
 source("./code/load-data-helpers.R")
 
@@ -26,84 +23,84 @@ out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "growth",
                           gform = NULL)
-write_feather(out, "./data/all_growth_data.feather")
+save(out, file = "./data/all_growth_data.R")
 
 # load fecundity data for all growth forms
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "fec",
                           gform = NULL)
-write_feather(out, "./data/all_fec_data.feather")
+save(out, file = "./data/all_fec_data.R")
 
 # load survival data for all growth forms
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "surv",
                           gform = NULL)
-write_feather(out, "./data/all_surv_data.feather")
+save(out, file = "./data/all_surv_data.R")
 
 # load growth data for trees, shrubs, herbs and palms
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "growth",
                           gform = "Tree")
-write_feather(out, "./data/tree_growth_data.feather")
+save(out, file = "./data/tree_growth_data.R")
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "growth",
                           gform = "Shrub")
-write_feather(out, "./data/shrub_growth_data.feather")
+save(out, file = "./data/shrub_growth_data.R")
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "growth",
                           gform = "Herbaceous perennial")
-write_feather(out, "./data/herb_growth_data.feather")
+save(out, file = "./data/herb_growth_data.R")
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "growth",
                           gform = "Palm")
-write_feather(out, "./data/palm_growth_data.feather")
+save(out, file = "./data/palm_growth_data.R")
 
 # load fecundity data for trees, shrubs, herbs and palms
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "fec",
                           gform = "Tree")
-write_feather(out, "./data/tree_fec_data.feather")
+save(out, file = "./data/tree_fec_data.R")
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "fec",
                           gform = "Shrub")
-write_feather(out, "./data/shrub_fec_data.feather")
+save(out, file = "./data/shrub_fec_data.R")
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "fec",
                           gform = "Herbaceous perennial")
-write_feather(out, "./data/herb_fec_data.feather")
+save(out, file = "./data/herb_fec_data.R")
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "fec",
                           gform = "Palm")
-write_feather(out, "./data/palm_fec_data.feather")
+save(out, file = "./data/palm_fec_data.R")
 
 # load survival data for trees, shrubs, herbs and palms
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "surv",
                           gform = "Tree")
-write_feather(out, "./data/tree_surv_data.feather")
+save(out, file = "./data/tree_surv_data.R")
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "surv",
                           gform = "Shrub")
-write_feather(out, "./data/shrub_surv_data.feather")
+save(out, file = "./data/shrub_surv_data.R")
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "surv",
                           gform = "Herbaceous perennial")
-write_feather(out, "./data/herb_surv_data.feather")
+save(out, file = "./data/herb_surv_data.R")
 out <- compadre_data_load(compadre_data = compadre_data,
                           trait_data = trait_data,
                           vital = "surv",
                           gform = "Palm")
-write_feather(out, "./data/palm_surv_data.feather")
+save(out, file = "./data/palm_surv_data.R")
